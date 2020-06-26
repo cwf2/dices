fc740e96e0f63b83e0aceea302078fa9a63ae466
Chris Forstall <cforstall@gmail.com>
2020-06-26 16:17:44 -0300

    change postgres settings for heroku

--- dices/settings.py
+++ dices/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+# for heroku deployment 
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
