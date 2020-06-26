563d1c7f75e41facb1d9d067522ff38fee139490
Chris Forstall <cforstall@gmail.com>
2020-06-26 11:17:18 -0300

    simple search works

--- speechdb/urls.py
+++ speechdb/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'speechdb'
 
 urlpatterns = [
-#    path('', views.IndexView.as_view(), name='index'),
+    path('', views.index, name='index'),
     path('characters', views.characters, name='characters'),
     path('clusters', views.clusters, name='clusters'),
+    path('speeches', views.speeches, name='speeches'),
+    path('search', views.search, name='search'),
 ]
--- speechdb/views.py
+++ speechdb/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.db.models import Q
 from .models import Character, Speech, SpeechCluster
 
 CTS_READER = 'https://scaife.perseus.org/reader/'
 
 def index(request):
-    return HttpResponse(render(request, 'speechdb/index.html'))
+    context = {'characters': Character.objects.all()}
+    return render(request, 'speechdb/index.html', context)
 
 def characters(request):
     context = {'characters': Character.objects.all()}
@@ -20,3 +22,42 @@ def clusters(request):
 def speeches(request):
     context = {'speeches': Speech.objects.all(), 'reader':CTS_READER}
     return render(request, 'speechdb/speeches.html', context)
+    
+def search(request):
+    '''Perform a search'''
+    
+    # sanitize inputs
+    valid_params = [
+        ('spkr_id', int),
+        ('addr_id', int),
+    ]
+    
+    params = {}
+    
+    for param, vtype in valid_params:
+        if param in request.GET:
+            val = request.GET[param][:256].strip()
+            if val != '':
+                try:
+                    params[param] = vtype(val)
+                except ValueError:
+                    pass
+    
+    # construct query
+    query = []
+    
+    # speaker by id
+    if 'spkr_id' in params:
+        query.append(Q(spkr__char=params['spkr_id']) | Q(spkr__disg=params['spkr_id']))
+
+    # addressee by id
+    if 'addr_id' in params:
+        query.append(Q(addr__char=params['addr_id']) | Q(addr__disg=params['addr_id']))
+    
+    # run query
+    results = Speech.objects.filter(*query)
+    
+    # render template
+    context = {'speeches': results}
+    return render(request, 'speechdb/speeches.html', context)
+    
