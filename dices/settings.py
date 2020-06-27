769fbfc59f9af401a09687819099cc57186fa401
Chris Forstall <cforstall@gmail.com>
2020-06-27 09:41:46 -0300

    add django_heroku

--- dices/settings.py
+++ dices/settings.py
@@ -127,3 +127,7 @@ STATIC_URL = '/static/'
 # for heroku deployment 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
